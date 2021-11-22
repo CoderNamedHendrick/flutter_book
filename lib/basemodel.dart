6286769b39316669b3b54db48159bd12354296c2
hendrick <sebastinesoacatp@gmail.com>
2021-11-22 16:55:59 +0100

    feat: basemodel and utils setup

---
+++ lib/basemodel.dart
@@ -0,0 +1,23 @@
+import 'package:scoped_model/scoped_model.dart';
+
+class BaseModel extends Model {
+  int stackIndex = 0;
+  List entityList = [];
+  var entityBeingEdited;
+  String? chosenDate;
+
+  void setChosenDate(String inDate) {
+    chosenDate = inDate;
+    notifyListeners();
+  }
+
+  void loadData(String inEntityType, dynamic inDatabse) async {
+    entityList = await inDatabse.getAll();
+    notifyListeners();
+  }
+
+  void setStackIndex(int inStackIndex) {
+    stackIndex = inStackIndex;
+    notifyListeners();
+  }
+}
---
+++ lib/notes/notes.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Notes extends StatelessWidget {
+  const Notes({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
